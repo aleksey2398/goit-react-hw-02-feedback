2eacf849684f908f7bd1adfa0a33b942cff194cf
Aleksey Bobrovskyi <87311512+aleksey2398@users.noreply.github.com>
2021-12-14 16:07:26 +0200

    next

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,63 @@
+import { Component } from "react";
+import styles from "./Feedback.module.css";
+import Section from "../Section/Section";
+import Statistics from "../Statistics/Statistics";
+import Options from "../Options/Options";
+
+
+class Feedback extends Component {
+    state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  
+    countFeedback = (name) => {
+      const currentState = name;
+      this.setState((prevState) => ({
+        [currentState]: prevState[currentState] + 1,
+      }));
+      this.countTotalFeedback();
+    };
+  
+    countTotalFeedback = () => {
+      const { good, neutral, bad } = this.state;
+      const total = good + neutral + bad;
+      return total;
+    };
+  
+    countPositiveFeedbackPercentage = () => {
+      const { good } = this.state;
+      const { countTotalFeedback } = this;
+      const total = countTotalFeedback();
+      const positive = Math.round((good / total) * 100);
+      return positive;
+    };
+  
+    render() {
+      const {
+        countFeedback,
+        countTotalFeedback,
+        countPositiveFeedbackPercentage,
+      } = this;
+      const { good, neutral, bad } = this.state;
+      const onLeaveFeedback = { countFeedback };
+      const names = Object.keys(this.state);
+      return (
+        <div className={styles.FeedbackForm}>
+          <Section title="Please leave feedback" />
+          <Options options={names} onLeaveFeedback={onLeaveFeedback} />
+          <Section title="Statistics" />
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positive={countPositiveFeedbackPercentage()}
+          />
+        </div>
+      );
+    }
+  }
+  
+  export default Feedback;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,27 @@
+import styles from "./Options.module.css";
+
+const Options = ({ onLeaveFeedback, options }) => {
+  const { countFeedback } = onLeaveFeedback;
+  const Button = options.map((name) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        name={name}
+        className={styles.FeedbackBtn}
+        onClick={() => countFeedback(name)}
+      >
+        {name}
+      </button>
+    );
+  });
+  return (
+    <>
+      <ul className={styles.FeedbackList}>
+        <li className={styles.FeedbackItems}>{Button}</li>
+      </ul>
+    </>
+  );
+};
+
+export default Options;
